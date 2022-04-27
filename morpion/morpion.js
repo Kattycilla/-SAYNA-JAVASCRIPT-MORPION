4fac4ec576889b3eb916ab5f525485a84173c019
Kattycilla <99472351+Kattycilla@users.noreply.github.com>
2022-04-27 15:54:45 +0200

    Add files via upload
    Mon javascript

---
+++ morpion/morpion.js
@@ -0,0 +1,4 @@
+let cases =[...document.getElementsByClassName("case")];
+let joueur= document.getElementById("gamer");
+let score1= document.getElementById("you");
+let score2= document.getElementById("cpu")
